c00624a316acf4e92366e6a080cb993b74d5c560
AndreyMlshv <samsonsamsonov59226@gmail.com>
2021-08-14 20:38:50 +0300

    в файле 1st.py находится пятая задача

---
+++ 1.3_ex.py
@@ -0,0 +1,7 @@
+#Первый урок, третья задача
+number = (input("Введите число:"))
+number_2 = number + number
+number_3 = number + number + number
+res_numb = int(number) + int(number_2) + int(number_3)
+print(res_numb)
+
